7bad8bcb1923f244d667c47895e6436e885dbb57
raihan2-byte <raihanalfarisi4@gmail.com>
2024-07-30 11:54:53 +0700

    fix index.html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,11 @@
-import "./App.css";
-import PortofolioContainer from "./PortofolioContainer/PortofolioContainer";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import PortofolioContainer from './PortofolioContainer/PortofolioContainer';
 
 function App() {
   return (
-    <section>
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<PortofolioContainer />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </section>
+    <div className='App'>
+      <PortofolioContainer/>
+    </div>
   );
 }
 
